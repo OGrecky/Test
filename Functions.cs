aca07e6b65439403530697224aeaf4eeaaa3cb65
User <example@example.com>
2020-10-11 14:03:24 +0300

    Changed Function Sum in Functions, removed everything from tests

--- Functions.cs
+++ Functions.cs
@@ -3,8 +3,12 @@ namespace UnitTest1
 {
     public class Functions
     {
-        public string SumStrings(string a, string b)
+        public int Sum(string x, string y)
         {
+            int a, b;
+            bool p = int.TryParse(x, out a);
+            bool q = int.TryParse(y, out b);
+            if (!(p && q)) return 0;
             return a + b;
         }
     }
--- Tests.cs
+++ Tests.cs
@@ -5,27 +5,5 @@ namespace UnitTest1
 {
     public class Tests
     {
-        const string a1 = "A";
-        const string b1 = "B";
-        const string a2 = "a";
-        const string b2 = "b";
-        const string a3 = "ABHJFDPBOIUSBVAURHV";
-        const string b3 = "FDJHVNJFLKDLKJFVNJJJFVKHBSKHV";
-        Functions tests = new Functions();
-        [Fact]
-        public void TestStrings1()
-        {
-            Assert.Equal(tests.SumStrings(a1, b1), a1 + b1);
-        }
-        [Fact]
-        public void TestStrings2()
-        {
-            Assert.Equal(tests.SumStrings(a2, b2), a2 + b2);
-        }
-        [Fact]
-        public void TestStrings3()
-        {
-            Assert.Equal(tests.SumStrings(a3, b3), a3 + b3);
-        }
     }
 }
