09f0cc45410a9b1ea1588d0b2559171931fcb658
User <example@example.com>
2020-10-11 14:19:16 +0300

    Wrote one test

--- Tests.cs
+++ Tests.cs
@@ -7,12 +7,20 @@ namespace UnitTest1
     {
         private const string Five = "5";
         private const string Three = "3";
+        Functions func = new Functions();
         [Fact]
-        public void WhenPeopleWriteSomeWeirdThing()
+        public void WhenPeopleWriteNormalThings()
         {
-            Functions func = new Functions();
             var result = func.Sum(Five, Three);
-
+            int correct = int.Parse(Five) + int.Parse(Three);
+            Assert.Equal(correct, result); // Firat parameter in Assert.Equal is 'Expected', the second is 'Actual'
+        }
+        private const string t2_1 = "ABCD"; // Test2_1st string
+        private const string t2_2 = "BHD";
+        [Fact]
+        public void WhenPeopleWriteWeirdThings()
+        {
+            //Write here now
         }
     }
 }
