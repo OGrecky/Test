84264331a3e37f5a69d3b9b2eca856dca1a7afe1
Ondushi <a_pyagay@mail.ru>
2020-10-11 14:14:55 +0300

    Add 2 strings

--- Tests.cs
+++ Tests.cs
@@ -5,5 +5,14 @@ namespace UnitTest1
 {
     public class Tests
     {
+        private const string Five = "5";
+        private const string Three = "3";
+        [Fact]
+        public void WhenPeopleWriteSomeWeirdThing()
+        {
+            Functions func = new Functions();
+            var result = func.Sum(Five, Three);
+
+        }
     }
 }
